86f3df3c92da839229bee2a4148d231a40eb7a90
luke9kim8 <luke9kim8@gmail.com>
2021-10-26 17:40:54 -0400

    add review exercise

---
+++ office-hour-queue/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Queue from './components/Queue';
+
+function App() {
+  return (
+    <div className="App">
+      <Queue/>
+    </div>
+  );
+}
+
+export default App;

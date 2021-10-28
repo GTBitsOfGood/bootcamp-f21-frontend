5155d1cfb68eb4a7c0d754b815b4a89d9026fcaa
luke9kim8 <luke9kim8@gmail.com>
2021-10-28 15:14:48 -0400

    add solution

--- office-hour-queue/src/components/Queue.jsx
+++ office-hour-queue/src/components/Queue.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import './Queue.css'
+
+const DISPLAY = ["Show all", "Labs only", "No labs only"]
 
 export default function Queue() {
-  const [students, setStudents] = useState([{name: "Luke", isHereForLab: true}]);
+  const [students, setStudents] = useState([]);
+  const [displayedStudents, setDisplayedStudents] = useState(students);
   const [name, setName] = useState("")
-  const [isHereForLab, setIsHereForLab] = useState("")
+  const [isHereForLab, setIsHereForLab] = useState(false)
+  const [displayOption, setDisplayOption] = useState(0)
 
   const nameChangeHandler = (e) => {
     setName(e.target.value)
@@ -14,24 +19,38 @@ export default function Queue() {
   }
 
   const submitHandler = () => {
+    console.log({name: name, isHereForLab: isHereForLab})
     const newStudents = [...students]
     newStudents.push({name: name, isHereForLab: isHereForLab})
-    setStudents(students)
+    setStudents(newStudents)
   }
+
+  const toggleDisplayOption = () => {
+    setDisplayOption((displayOption + 1) % DISPLAY.length)
+  }
+
+  useEffect(() => {
+    const newStudents = students.filter(
+      student => displayOption == 1 && student.isHereForLab 
+                || displayOption == 2 && !student.isHereForLab 
+                || displayOption == 0)
+    setDisplayedStudents(newStudents)
+  }, [displayOption, students.length])
+
 
   return (
     <div>
       <div>
         <p> NAME: <input type="text" onChange={nameChangeHandler}/></p>
         <p> LAB: <input type="checkbox" onChange={labChangeHandler}/></p>
-        <button onClick={submitHandler}> SUBMIT </button>
-        <button> LAB DISPLAY </button>
-        <button> NON LAB DISPLAY </button>
+        <button onClick={submitHandler}> Sign up </button>
+        <button onClick={toggleDisplayOption}> {DISPLAY[displayOption]} </button>
       </div>
       {
-        // students.map(student => <p> {student.name} </p>)
-        students.map((student, idx) => <p key={idx}> {student.name} </p>)
+        displayedStudents.map((student, idx) => <p key={idx} className={student.isHereForLab ? "bold" : ""}> {student.name} </p>)
       }
     </div>
   )
+
+
 }

efeb9cae1b8080a1553913404a93c428702d7137
luke9kim8 <luke9kim8@gmail.com>
2021-10-31 01:46:00 -0400

    add exercises

--- src/pages/browse/index.jsx
+++ src/pages/browse/index.jsx
@@ -1,18 +1,3 @@
 import BrowseScreen from "../../screens/Browse/BrowseScreen";
-// export getServerSideProps;
-
-export async function getServerSideProps() {
-  // context is useful to learn about which routes it indexed
-  const res  = await fetch("http://localhost:3001/animals")
-  const animals = await res.json()
-  if (!animals) {
-    return {
-      notFound: true,
-    }
-  }
-  return {
-    props: { animals }
-  }
-}
 
 export default BrowseScreen;
